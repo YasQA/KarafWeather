b05a3c76dab664b82f0197ad2e8e2c238d39da59
Yaroslav Lebid <yaroslav.lebid@gmail.com>
2021-08-30 19:19:40 +0300

    lombok usageadded, pom files fixes(versions to main pom, redundant buil declaration removed from main pom),  xmls renaming

--- weather-dto/src/main/java/com/yas/dto/WeatherDto.java
+++ weather-dto/src/main/java/com/yas/dto/WeatherDto.java
@@ -1,36 +1,21 @@
 package com.yas.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class WeatherDto {
     private String name;
     private Main main;
 
-    public WeatherDto() {
-    }
-
-    public WeatherDto(String name, Main main) {
-        this.name = name;
-        this.main = main;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Main getMain() {
-        return main;
-    }
-
-    public void setMain(Main main) {
-        this.main = main;
-    }
-
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Main {
         private double temp;
@@ -39,66 +24,6 @@ public class WeatherDto {
         private double temp_max;
         private double pressure;
         private double humidity;
-
-        public Main() {
-        }
-
-        public Main(double temp, double feels_like, double temp_min, double temp_max, double pressure, double humidity) {
-            this.temp = temp;
-            this.feels_like = feels_like;
-            this.temp_min = temp_min;
-            this.temp_max = temp_max;
-            this.pressure = pressure;
-            this.humidity = humidity;
-        }
-
-        public double getTemp() {
-            return temp;
-        }
-
-        public void setTemp(double temp) {
-            this.temp = temp;
-        }
-
-        public double getFeels_like() {
-            return feels_like;
-        }
-
-        public void setFeels_like(double feels_like) {
-            this.feels_like = feels_like;
-        }
-
-        public double getTemp_min() {
-            return temp_min;
-        }
-
-        public void setTemp_min(double temp_min) {
-            this.temp_min = temp_min;
-        }
-
-        public double getTemp_max() {
-            return temp_max;
-        }
-
-        public void setTemp_max(double temp_max) {
-            this.temp_max = temp_max;
-        }
-
-        public double getPressure() {
-            return pressure;
-        }
-
-        public void setPressure(double pressure) {
-            this.pressure = pressure;
-        }
-
-        public double getHumidity() {
-            return humidity;
-        }
-
-        public void setHumidity(double humidity) {
-            this.humidity = humidity;
-        }
     }
 
 }
